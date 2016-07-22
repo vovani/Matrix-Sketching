44a3253fe6ddff09977c89f5cdf91ca4c35c43a6
Vladimir Anisimov <vovaani@gmail.com>
2016-07-22 12:44:46 +0300

    indent

--- random_features.m
+++ random_features.m
@@ -1,11 +1,11 @@
 function [ Z, phi ] = random_features( s, dataset )
-d = size(dataset.X, 2);
-gen_random_phi = @(w, b) (@(x) sqrt(2 / s) * cos((x * w)' + repmat(b, [1, size(x, 1)])));
+    d = size(dataset.X, 2);
+    gen_random_phi = @(w, b) (@(x) sqrt(2 / s) * cos((x * w)' + repmat(b, [1, size(x, 1)])));
 
-w = mvnrnd(zeros(s, d), (dataset.sigma ^ -2) * eye(d))';
-b = 2 * pi * rand(s, 1);
+    w = mvnrnd(zeros(s, d), (dataset.sigma ^ -2) * eye(d))';
+    b = 2 * pi * rand(s, 1);
 
-phi = gen_random_phi(w, b);
-Z = phi(dataset.X)';
+    phi = gen_random_phi(w, b);
+    Z = phi(dataset.X)';
 end
 
