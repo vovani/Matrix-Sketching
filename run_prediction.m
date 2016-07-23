be3c1a33392d5e5a2dda97d05a3216938c01d00c
Vladimir Anisimov <vovaani@gmail.com>
2016-07-23 16:49:09 +0300

    One to One classification

--- run_prediction.m
+++ run_prediction.m
@@ -5,15 +5,29 @@ function err = run_prediction( Z, phi, s, dataset )
         err = norm(reg - dataset.Yt, 2) / norm( dataset.Yt, 2 );
     else
         classes = unique(dataset.L)';
-        f = zeros(length(classes), s);
+        iter = 0;
         for i = 1:length(classes)
-            c = classes(i);
-            one2rest = 2 * (dataset.L == c) - 1;
-            f(i,:) = (Z' * Z + dataset.lambda * eye(s)) \ (Z' * one2rest);
+            for j = i + 1: length(classes)
+                ci = classes(i);
+                cj = classes(j);
+                Zi = Z(dataset.L == ci, :);
+                Zj = Z(dataset.L == cj, :);
+                A = Zi' * Zi + Zj' * Zj;
+                y = Zi' * ones(size(Zi,1), 1) - Zj' * ones(size(Zj,1), 1);
+                
+                iter = iter + 1;
+                in(iter, :) = [ci,cj];
+                f(iter,:) = (A + dataset.lambda * eye(s)) \ y;
+            end
         end 
 
-        [~, pred] =  max(f * phi(dataset.Xt));
-        err = nnz(classes(pred) ~= dataset.Lt') / length(dataset.Lt);
+        pos = repmat(in(:, 1), [1 size(dataset.Xt,1)]);
+        neg = repmat(in(:, 2), [1 size(dataset.Xt,1)]);
+        
+        t = f * phi(dataset.Xt);
+        
+        pred = mode(neg .* (t < 0) + pos.* (t>0), size(t, 1));
+        err = nnz(pred ~= dataset.Lt') / length(dataset.Lt);
     end 
 end
 
